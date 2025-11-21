158724a4795d58b5398b4e3e6a24b4865821f22f
B-Ventures <hello@bosstsc.com>
2025-11-22 00:20:01 +0300

    updated files

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,11 @@ export default defineConfig({
   base: './', // Critical for GitHub Pages deployment to work on subpaths
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: true,
+    chunkSizeWarningLimit: 1600,
+  },
+  // Define process.env to prevent "process is not defined" errors in the browser
+  define: {
+    'process.env': {} 
   }
 });
